7a34f437bad23212ee266f8ddf61b40d19acda71
andresperezfirigua <andresperezfirigua@gmail.com>
2020-11-22 17:59:53 -0500

    End of section 10

---
+++ API/DTOs/MemberUpdateDTO.cs
@@ -0,0 +1,11 @@
+namespace API.DTOs
+{
+    public class MemberUpdateDTO
+    {
+        public string Introduction { get; set; }
+        public string Specialty { get; set; }
+        public string Experience { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
